2ccc3ea605e9c5ffd1bc0509be7e6d16bd20bd32
ubbagg <garvgarg1902@gmail.com>
2024-06-19 00:00:45 +0530

    login vhangers

--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    document.getElementById('loginForm').addEventListener('submit', async function(event) {
         event.preventDefault();
 
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         console.log(`Submitting login for email: ${email}`);
 
-        try{
-            const response =await fetch('/api/users/login', {
+        try {
+            const response = await fetch('/api/users/login', {
                 method: 'POST',
-                headers:{
-                    'Content-Type':'application/json'
+                headers: {
+                    'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({email, password})
+                body: JSON.stringify({ email, password })
             });
-            console.log('Response recieved', response);
-            if (response.ok) {
-                const result = await response.json();
-                console.log('User logged in:', result);
-                window.location.href = 'home.html';
-            } else {
-                console.log('Response status not OK:', response.status);
-                console.log('Attempting to read error text');
+
+            if (!response.ok) {
                 const errorText = await response.text();
-                console.error('Failed to log in:', response.status, errorText);
-                alert('Failed to log in:' + errorText);
+                throw new Error(`Failed to log in: ${response.status} - ${errorText}`);
             }
-        }catch (error){
-            console.error('Error:',error);
-            alert('An error occured');
+
+            const result = await response.json();
+            console.log('User logged in:', result);
+            window.location.href = 'home.html';
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred while logging in');
         }
     });
 });
-
-//to get the user details run"node listUsers.js"
